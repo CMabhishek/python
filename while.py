8eb3334f7cfed0a331e9b668e42b7289bb29dcae
CMabhishek <167234783+CMabhishek@users.noreply.github.com>
2024-04-18 22:41:24 +0530

    Create while.py

---
+++ while.py
@@ -0,0 +1,69 @@
+#while loop
+print("last line get exiguted")
+
+i = 23
+while i < 25:
+    print("statement-1")
+    print("statement-2")
+    print("statement-3")
+    i  = i + 1
+print("1st line get exiguted")
+print("2nd line get exiguted")
+
+
+output:
+
+last line get exiguted
+statement-1
+statement-2
+statement-3
+statement-1
+statement-2
+statement-3
+1st line get exiguted
+2nd line get exiguted
+
+
+
+	i = 55
+while i < 97:
+    i= i + 1
+    print(i)
+24>>
+   i = 1224
+while i > 995:
+    i= i - 1
+    print(i)
+
+
+or
+
+i = 0
+while i < 10:
+    i = i+1
+    j= i * 5
+    print(j)
+
+
+# 7th table rin revers
+     i=11
+	while i > 1:
+    i = i-1
+    j= i * 7
+    print(j)
+
+# 12 to 56 even no
+	i = 12
+while i <= 56:   
+    if i%2==0:
+        print(i)
+    i=i+2 
+	
+
+or
+
+i = 12
+while i <= 56:   
+    
+    print(i)
+    i=i+2    
